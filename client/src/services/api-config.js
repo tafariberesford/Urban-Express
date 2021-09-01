8b1067c44622e2a630391a6a15bcb7a8734ecbaf
Tafari Beresford <tafariberesford@Tafaris-MBP.fios-router.home>
2021-09-01 09:42:37 -0400

    added

--- client/src/services/api-config.js
+++ client/src/services/api-config.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const baseUrl =
   process.env.NODE_ENV === 'production'
-    ? 'deployed link will go here'
+    ? 'https://urban-express-api.herokuapp.com/'
     : 'http://localhost:3000'
 
 const api = axios.create({
