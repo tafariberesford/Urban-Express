bfb5fe02d2159fa68aed8c34091fa4dcfaef28cf
Tafari Beresford <tafariberesford@Tafaris-MBP.fios-router.home>
2021-09-01 02:07:43 -0400

    added framer with two pics

--- client/src/screens/Homepage.jsx
+++ client/src/screens/Homepage.jsx
@@ -1,53 +1,177 @@
-import react from 'react';
-import { useState } from 'react';
+import { motion } from "framer-motion";
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
-export default function Homepage() {
+const media = {
+  desktop: `@media screen and (max-width: 600px)`
+}
 
-  const slideData = [
-    {
-      image: 'https://i1.sndcdn.com/artworks-7igI9cWrpvt7-0-t500x500.jpg'
-    },
-  
-    {
-      image: 'https://i.scdn.co/image/ab67616d0000b2736d8b9f3e7337f6bff76ceff6'
-    },
-  
-    {
-      image: 'https://i1.sndcdn.com/artworks-sdNygCXd2zbGK1Yp-8tV2LA-t500x500.jpg'
-    },
-  
-    {
-      image: 'https://dancehallarena.com/wp-content/uploads/2021/05/NICKI-MINAJ-SKILLIBENG-CROCODILE-TEETH-REMIX.jpg'
-    },
-  
-    {
-      image: 'https://i1.sndcdn.com/artworks-mDsLDbWWUf9BuTEp-zCRoKQ-t500x500.jpg'
-    },
+const Section = styled.section`
+height: 100vh;
+display: flex;
+justify-content: center;
+align-items: center;
+background: #131313;
+`;
 
-    {
-      image: 'https://m.media-amazon.com/images/I/81Uj3NtUuhL._SS500_.jpg'
-    },
-  
-    {
-      image: 'https://hipradar.net/wp-content/uploads/2021/07/Kanye-West-DONDA-Album.jpg'
-    },
+const Container = styled.div`
+display: grid;
+grid-template-columns: 1fr 1fr;
+height: 100vh;
+padding: 3rem calc((100vw - 1300px) /2);
 
-    {
-      image: 'https://i.scdn.co/image/ab67616d0000b273ad89f82d9243ed88a6d68787'
-    },
+}
 
-    {
-      image: 'https://i1.sndcdn.com/artworks-jLajqkRdbRJKAsfC-RxRFVA-t500x500.jpg'
-    },
+`;
 
-    {
-      image: 'https://images.genius.com/08e089999a0472c90a21c4e637019440.1000x1000x1.png'
-    }
-  ];
+const ColumnLeft = styled.div`
+display: flex;
+color: #fff;
+flex-direction: column;
+justify-content: center;
+align-items: flex-start;
+padding: 5rem 2rem;
 
+
+p {
+  margin: 2rem 0;
+  font-size: 4rem;
+  line-height: 1.1;
+  @media screen and (max-width: 600px) {
+    font-size: 15px;
+  }
+}
+`;
+
+
+const Button = styled(motion.button)`
+padding: 1rem 3rem;
+font-size: 1rem;
+border: 2px solid #fff;
+border-radius: 4px;
+outline: none;
+cursor: pointer;
+color: white;
+background: transparent;
+`;
+
+const Image = styled(motion.img)`
+position: absolute;
+width: 100%;
+height: 100%;
+// height: 30px;
+max-width: 350px;
+max-height: 350px;
+
+`;
+
+const ColumnRight = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+padding: 2rem;
+position: relative;
+
+@media screen and (max-width: 768px) {
+  grid-template-columns: 1fr;
+}
+
+${Image}:nth-child(1) {
+  bottom: 65px;
+  left: 250px;
+  border-radius: 50%;
+
+  @media screen and (max-width: 600px) {
+    bottom: 370px;
+    left: 270px;
+  }
+}
+${Image}:nth-child(2) {
+  bottom: 65px;
+  left: 320px;
+  border-radius: 50%;
+
+  @media screen and (max-width: 600px) {
+    bottom: 270px;
+    left: 270px;
+  }
+}
+${Image}:nth-child(3) {
+  bottom: 65px;
+  left: 390px;
+  border-radius: 50%;
+
+  @media screen and (max-width: 600px) {
+    bottom: 170px;
+    left: 270px;
+  }
+}
+${Image}:nth-child(4) {
+  bottom: 65px;
+  left: 460px;
+  border-radius: 50%;
+
+  @media screen and (max-width: 600px) {
+    bottom: 70px;
+    left: 270px;
+  }
+}
+`;
+
+const ImgSlide = () => {
+  const motionLeft = {
+    hidden: { opacity: 0, x: -100 },
+    visible: {opacity: 1, x: 0}
+  }
   return (
-    <div className='home'>
-      <h1>Home</h1>
+    <div className="min-h-screen">
+    <Section>
+      <Container>
+        <ColumnLeft>
+          <motion.h1
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+            
+            >
+            Urban Express
+          </motion.h1
+          
+          
+          >
+          <motion.p
+            variants={motionLeft}
+            initial='hidden'
+            animate='visible'
+            transition={{duration: 4}}
+          
+          >Hip-Hop's Very Best</motion.p>
+          <Link to="/sign-up"><Button
+          whileTap={{scale: 1}}
+          whileHover={{scale: 1.2}}
+          
+          >Sign Up</Button></Link>
+          <Link to="/sign-in"><Button
+            whileTap={{ scale: 1 }}
+            whileHover={{scale: 1.2}}
+          >
+            Sign In</Button></Link>
+          <ColumnRight>
+            <Image src={'https://i.pinimg.com/736x/d4/cf/be/d4cfbe343199c8587a6a3eede115acc3.jpg'} alt="images"
+              drag={true}
+            />
+            <Image src={'https://lastfm.freetls.fastly.net/i/u/770x0/334d050af3482fbc0852be9a55d4eebc.jpg'} alt="images"
+              drag={true} />
+            <Image src={'https://img.freepik.com/free-vector/flat-hotel-building-background_23-2148152507.jpg?size=338&ext=jpg&ga=GA1.2.2099647507.1627689600'} alt="images"
+              drag={true} />
+            <Image src={'https://img.freepik.com/free-vector/earth-globe-design_1053-479.jpg?size=338&ext=jpg'} alt="images"
+              drag={true} />
+          </ColumnRight>
+        </ColumnLeft>
+      </Container>
+    </Section>
     </div>
   )
-};
+}
+
+export default ImgSlide
